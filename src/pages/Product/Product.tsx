ad275580d1212568dd0785f72890497154c9f8b1
johnpaulsale <iamjohnpaulsale12@gmail.com>
2025-11-07 02:02:04 +0800

    feat: create Product Page

---
+++ src/pages/Product/Product.tsx
@@ -0,0 +1,14 @@
+import Container from "../../components/Container/Container"
+import MainLayout from "../../layouts/MainLayout"
+
+const Product = () => {
+    return (
+        <MainLayout>
+            <Container>
+                Individual Product
+            </Container>
+        </MainLayout>
+    )
+}
+
+export default Product
