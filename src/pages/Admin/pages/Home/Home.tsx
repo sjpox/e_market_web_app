feff198e0fd70d54f4f845f2e76f899c14b2e97b
johnpaulsale <iamjohnpaulsale12@gmail.com>
2025-11-24 21:31:14 +0800

    feat: integrate hook-form

--- src/pages/Admin/pages/Home/Home.tsx
+++ src/pages/Admin/pages/Home/Home.tsx
@@ -1,9 +1,25 @@
+import { useForm, type SubmitHandler } from "react-hook-form";
 import Container from "../../../../components/Container/Container";
 
+interface IFormInput {
+    firstName: string
+    gender: string
+}
+
 const Home = () => {
+    // the admin app should be in different project
+    const { register, handleSubmit } = useForm<IFormInput>()
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
     return (
         <Container>
             <h1>Admin Home Page</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="firstName">First Name</label><br/>
+                <input {...register("firstName")} className="border-2"/><br/>
+                <label htmlFor="gender">Gender</label><br/>
+                <input {...register("gender")} className="border-2"/><br/>
+                <input type="submit" />
+            </form>
         </Container>
     )
 }
